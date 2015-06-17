5565415f0e35305312fcb7bbee54bf2b0144fd00
Jitan Elis <jitan.elis@gmail.com>
2015-06-17 15:13:34 +0200

    style: Add icon and basic ui components

--- app/src/main/java/nu/jitan/spotifystreamer/MainActivity.java
+++ app/src/main/java/nu/jitan/spotifystreamer/MainActivity.java
@@ -1,11 +1,11 @@
 package nu.jitan.spotifystreamer;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
